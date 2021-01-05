a886b977229ae63a53d0cea0ec1562e8ee6db7bf
Yago Teixeira <yagaot@gmail.com>
2021-01-04 21:17:23 -0300

    creating envs

--- lisp.cpp
+++ lisp.cpp
@@ -7,16 +7,13 @@
 #include <cstring>
 #include <cstdlib>
 #include <iostream>
+#include <unordered_map>
 
 class Object
 {
 public:
 	Object() : ref_count(0) {}
-
-	virtual ~Object()
-	{
-		std::cout << "dealloc" << std::endl;
-	}
+	virtual ~Object(){}
 
 	const Object* get() const
 	{
@@ -45,7 +42,7 @@ template<class T>
 class ObjectPtr
 {
 public:
-	ObjectPtr() : Object(nullptr) {}
+	ObjectPtr() : object(nullptr) {}
 	ObjectPtr(T *object) : object(nullptr)
 	{
 		get(object);
@@ -70,6 +67,11 @@ public:
 		return object;
 	}
 
+	operator bool () const
+	{
+		return object != nullptr;
+	}
+
 private:
 	void get(T *object)
 	{
@@ -86,6 +88,25 @@ private:
 	}
 
 	T *object;
+};
+
+class Value;
+class Env;
+class ValueList;
+typedef ObjectPtr<Value> ValuePtr;
+typedef ObjectPtr<Env> EnvPtr;
+#define CREATE_VALUE(x) ValuePtr(new Value(x))
+#define NIL_VALUE ValuePtr(new Value())
+#define IS_NIL(v) (v)->type == Value::TYPE::NIL
+using ValueListIter = std::vector<ValuePtr>::iterator;
+typedef ObjectPtr<ValueList> ValueListPtr;
+typedef std::function<ValuePtr(EnvPtr)> Native;
+typedef std::unordered_map<std::string, ValuePtr> BindsMap;
+
+struct Arg
+{
+	std::string name;
+	bool varg = false;
 };
 
 class Value : public Object
@@ -110,17 +131,17 @@ public:
 		long int number;
 		float real;
 		std::string string;
-		std::vector<ObjectPtr<Value>> *list;
+		ValueListPtr list;
+		Native native;
 	};
 
 	Value() : type(TYPE::NIL), Object() {}
 	Value(int x) : number(x), type(TYPE::INTEGER), Object() {}
 	Value(float x) : real(x), type(TYPE::REAL), Object() {}
-	Value(std::vector<ObjectPtr<Value>> *l) : list(l), type(TYPE::LIST), Object() {}
+	Value(ValueListPtr l) : list(l), type(TYPE::LIST), Object() {}
 	Value(const char *s) : string(std::string(s)), type(TYPE::STRING), Object() {}
-	~Value() {
-		std::cout << "value(" << as_string() << "): ";
-	};
+	Value(Native l) : native(l), type(TYPE::NATIVE), Object() {}
+	~Value() {};
 	Value(const Value&v) : type(v.type){
 		switch(type)
 		{
@@ -132,6 +153,9 @@ public:
 			break;
 		case TYPE::LIST:
 			list = v.list;
+			break;
+		case TYPE::NATIVE:
+			native = v.native;
 			break;
 		}
 	};
@@ -149,60 +173,124 @@ public:
 	}
 };
 
-using ValuePtr = ObjectPtr<Value>;
-#define CREATE_VALUE(x) ValuePtr(new Value(x))
-#define NIL_VALUE ValuePtr(new Value())
-using ValueList = std::vector<ValuePtr>;
-using ValueListIter = ValueList::iterator;
-using BuildIn = std::function<ValuePtr(ValueListIter, ValueListIter)>;
-std::map<std::string, BuildIn> library;
+class ValueList : public Object
+{
+public:
+	ValueList() : Object() {}
+	ValueList(std::initializer_list<ValuePtr> v) : content(v.begin(), v.end()), Object() {}
+	ValueList(ValueListIter begin, ValueListIter end) : content(begin, end), Object() {}
 
-ValuePtr eval(ValueList *s);
+	std::vector<ValuePtr> *operator->()
+	{
+		return &content;
+	}
 
-ValuePtr eval(ValuePtr c)
+	std::vector<ValuePtr> *raw()
+	{
+		return &content;
+	}
+
+	ValueListIter begin()
+	{
+		return content.begin();
+	}
+
+	ValueListIter end()
+	{
+		return content.end();
+	}
+
+private:
+	std::vector<ValuePtr> content;
+};
+
+class Env : public Object
+{
+public:
+	Env() : parent(EnvPtr()) , Object() {}
+
+	Env(EnvPtr parent) : parent(parent), Object() {}
+
+	BindsMap binds;
+	EnvPtr parent;
+
+	ValuePtr get_value(const std::string &symbol)
+	{
+		BindsMap::iterator it = binds.find(symbol);
+		if(it != binds.end())
+			return it->second;
+		if(parent)
+			return parent->get_value(symbol);
+		return NIL_VALUE;
+	}
+
+	void set_value(const std::string &symbol, const ValuePtr &value)
+	{
+		binds[symbol] = value;
+	}
+};
+
+std::map<std::string, Native> library;
+
+ValuePtr eval(ValueListPtr s, EnvPtr env);
+
+ValuePtr eval(ValuePtr c, EnvPtr env)
 {
 	if(c->type == Value::TYPE::LIST)
-		return eval(c->list);
+		return eval(c->list, env);
 	return c;
 }
 
-ValuePtr eval(ValueList *s)
+ValuePtr eval(ValueListPtr s, EnvPtr env)
 {
 	assert(s->begin() != s->end());
-	ValuePtr f_name = eval(*s->begin());
-	const auto f_it = library.find(f_name->as_string());
-	assert(f_it != library.end());
-	return f_it->second(++s->begin(), s->end());
+	ValuePtr f_name = eval(*s->begin(), env);
+	const auto f_it = env->get_value(f_name->as_string());
+	assert(!IS_NIL(f_it));
+	ValueListIter pos = s->begin();
+	ValueListPtr l(new ValueList{++pos, s->end()});
+	EnvPtr f_env(new Env(env));
+	f_env->set_value("args", CREATE_VALUE(l));
+	return f_it->native(f_env);
 }
 
 int main()
 {
-	library["print"] = [](ValueListIter pos, ValueListIter end) {
+	EnvPtr root(new Env());
+	Native print_f = [](EnvPtr env) {
+		const ValuePtr args = env->get_value("args");
+		ValueListPtr argl = args->list;
+		ValueListIter it = argl->begin();
 		std::string s;
-		while(pos != end) s += eval(*pos++)->as_string();
+		while(it != argl->end()) s += eval(*it++, env)->as_string();
 		std::cout << s << std::endl;
 		return NIL_VALUE;
 	};
+	root->set_value("print", CREATE_VALUE(print_f));
 
-	library["+"] = [](ValueListIter pos, ValueListIter end) {
+	Native sum_f = [](EnvPtr env) {
+		const ValuePtr args = env->get_value("args");
+		ValueListPtr argl = args->list;
+		ValueListIter it = argl->begin();
 		ValuePtr v = CREATE_VALUE(0);
-		while(pos != end) v->number += eval(*pos++)->number;
+		while(it != argl->end()) v->number += eval(*it++, env)->number;
 		return v;
 	};
+	root->set_value("+", CREATE_VALUE(sum_f));
 
-	ValueList sum{
+	ValueListPtr sum(new ValueList{
 		CREATE_VALUE("+"),
 		CREATE_VALUE(1),
 		CREATE_VALUE(2)
-	};
+		});
 
-	ValueList prog{
+	ValueListPtr prog(new ValueList{
 		CREATE_VALUE("print"),
 		CREATE_VALUE("1 + 2 = "),
-		CREATE_VALUE(&sum)
-	};
+		CREATE_VALUE(sum)
+		});
 
-	eval(&prog);
+	eval(prog, root);
 
 	return 0;
 }
